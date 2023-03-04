097cb5771b8a626f5ef08c05492250f10417c4dd
odanzhou <odanzhou@gmail.com>
2023-03-05 00:48:35 +0800

    feat: 错误处理

--- src/raw-loader.js
+++ src/raw-loader.js
@@ -8,5 +8,8 @@ module.exports = function(source) {
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029')
   // 将文件转化为模块
-  return `export default ${json}`
+  const resource = `export default ${json}`
+  // throw new Error('Error')
+  // return resource
+  this.callback(null, resource)
 }
