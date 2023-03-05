3c2cbc158ddad3af7a18168dbaf473ffb60b8430
odanzhou <odanzhou@gmail.com>
2023-03-05 12:57:28 +0800

    feat: 打包文件输出

--- src/emit-loader.js
+++ src/emit-loader.js
@@ -1,12 +1,14 @@
 const loaderUtils = require('loader-utils')
 
 module.exports = function(source) {
+  const options = loaderUtils.getOptions(this) || {}
   console.log('emitFile', source)
   const url = loaderUtils.interpolateName(this, '[name].txt', source)
+  console.log('url', url, this.emitFile)
   // webpack 中具有这个方法
   if(this.emitFile) {
     this.emitFile(url, source);
-  } else {
-    return source;
   }
+  const esModule = typeof options.esModule !== 'undefined' ? options.esModule : true;
+  return `${esModule ? 'export default' : 'module.exports =' } ${url}`
 }
