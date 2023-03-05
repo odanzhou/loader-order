d81ad6ccc7af96c1423da54d91419405a874a1bb
odanzhou <odanzhou@gmail.com>
2023-03-05 13:50:50 +0800

    feat: 雪碧图loader, 雪碧图处理包

--- src/emit-loader.js
+++ src/emit-loader.js
@@ -4,11 +4,11 @@ module.exports = function(source) {
   const options = loaderUtils.getOptions(this) || {}
   console.log('emitFile', source)
   const url = loaderUtils.interpolateName(this, '[name].txt', source)
-  console.log('url', url, this.emitFile)
+  console.log('urlurlurlurlurlurl', url, this.emitFile)
   // webpack 中具有这个方法
   if(this.emitFile) {
     this.emitFile(url, source);
   }
   const esModule = typeof options.esModule !== 'undefined' ? options.esModule : true;
-  return `${esModule ? 'export default' : 'module.exports =' } ${url}`
+  return `${esModule ? 'export default' : 'module.exports =' } './${url}'`
 }
---
+++ src/sprites-loader.js
@@ -0,0 +1,17 @@
+// 雪碧图loader
+
+const Spritesmith = require('spritesmith')
+const fs = require('fs')
+const path = require('path')
+
+const sprites = ['./src/images/o.jpg', './src/images/cat.jpg']
+
+Spritesmith.run({ src: sprites}, (err, result) => {
+  if(err) {
+    console.error(err)
+  }
+  console.log('result', result)
+  console.log(result.image)
+  console.log(result.coordinates)
+  console.log(result.properties)
+})
